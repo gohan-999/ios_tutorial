3aa3c6436e941247543456d72143538f428cccc7
InoueHiroyuki <omega999kurahuto@yahoo.co.jp>
2019-05-16 01:17:16 +0900

    Initial Commit

---
+++ layout-sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  layout-sample
+//
+//  Created by 井上裕之 on 2019/05/16.
+//  Copyright © 2019年 gohan999. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

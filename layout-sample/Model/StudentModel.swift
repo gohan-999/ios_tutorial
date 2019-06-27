796f42ab91502d3e2b35573abc25e774a2c261f6
hiroyuki.inoue <hiroyuki.inoue@livesense.co.jp>
2019-06-27 13:55:24 +0900

    Modify モデルサンプル追加

---
+++ layout-sample/Model/StudentModel.swift
@@ -0,0 +1,19 @@
+//import Foundation
+//import RealmSwift
+//
+//class StudentModel: Object {
+//    @objc dynamic var id = -1
+//    @objc dynamic var email = ""
+//    @objc dynamic var lastName = ""
+//    @objc dynamic var firstName = ""
+//    @objc dynamic var gender = ""
+//    @objc dynamic var tel = ""
+//    @objc dynamic var zipCode1 = ""
+//    @objc dynamic var zipCode2 = ""
+//    @objc dynamic var address: String?
+//    @objc dynamic var school: StudentSchoolModel?
+//
+//    override static func primaryKey() -> String {
+//        return "id"
+//    }
+//}
---
+++ layout-sample/Model/StudentSchoolModel.swift
@@ -0,0 +1,17 @@
+//import Foundation
+//import RealmSwift
+//
+//class StudentSchoolModel: Object {
+//    @objc dynamic var studentId = -1
+//    var scheduledGraduationYear = RealmOptional<Int>()
+//    var scheduledGraduationMonth = RealmOptional<Int>()
+//    var schoolId = RealmOptional<Int>()
+//    @objc dynamic var schoolName = ""
+//    @objc dynamic var schoolType = ""
+//    @objc dynamic var graduatedHighSchool: String?
+//    @objc dynamic var schoolRank: String?
+//
+//    override static func primaryKey() -> String {
+//        return "studentId"
+//    }
+//}

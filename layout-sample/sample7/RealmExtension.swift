bbb26a83968af4cabe00fc1a24dbfb0e1c18c3f0
hiroyuki.inoue <hiroyuki.inoue@livesense.co.jp>
2019-06-27 13:15:15 +0900

    Add RealmExtensionのベース作成

---
+++ layout-sample/sample7/RealmExtension.swift
@@ -0,0 +1,85 @@
+//import Foundation
+//import RealmSwift
+//
+//protocol RealmProtocol { }
+//extension Object: RealmProtocol { }
+//
+//extension RealmProtocol where Self: Object {
+//
+//    static func get() -> Results<Self>? {
+//        do {
+//            let realm = try Realm()
+//            return realm.objects(self)
+//        } catch {
+//            return nil
+//        }
+//    }
+//
+//    static func get(_ filter: NSPredicate) -> Results<Self>? {
+//        do {
+//            let realm = try Realm()
+//            return realm.objects(self).filter(filter)
+//        } catch {
+//            return nil
+//        }
+//    }
+//
+//    static func get<KeyType>(forPrimaryKey key: KeyType) -> Self? {
+//        do {
+//            let realm = try Realm()
+//            return realm.object(ofType: self, forPrimaryKey: key)
+//        } catch {
+//            return nil
+//        }
+//    }
+//
+//    static func delete() {
+//        do {
+//            let realm = try Realm()
+//            try realm.write {
+//                if let results = get(), !results.isEmpty {
+//                    realm.delete(results)
+//                }
+//            }
+//        } catch let error as NSError {
+//            Logger.error(error)
+//        }
+//    }
+//
+//    static func delete(_ filter: NSPredicate) {
+//        do {
+//            let realm = try Realm()
+//            try realm.write {
+//                if let results = get(filter), !results.isEmpty {
+//                    realm.delete(results)
+//                }
+//            }
+//        } catch let error as NSError {
+//            Logger.error(error)
+//        }
+//    }
+//
+//    static func delete<KeyType>(forPrimaryKey key: KeyType) {
+//        do {
+//            let realm = try Realm()
+//            try realm.write {
+//                if let object = get(forPrimaryKey: key) {
+//                    realm.delete(object)
+//                }
+//            }
+//        } catch let error as NSError {
+//            Logger.error(error)
+//        }
+//    }
+//
+//    func add(update: Bool = false) {
+//        do {
+//            let realm = try Realm()
+//            try realm.write {
+//                realm.add(self, update: update)
+//            }
+//        } catch let error as NSError {
+//            Logger.error(error)
+//        }
+//    }
+//}
